78eb2ddee79d9e5b0afe44857ad8d1051cfd6820
Gopal2456 <gopalmahajan3392@gmail.com>
2026-07-30 13:56:44 +0530

    Created api for social account creation

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import socialAuthRouter from "./routes/socialAuthRoute.js";
 
 const app = express();
 
@@ -18,6 +20,8 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
 
+app.use('/api/auth', authRouter)
+app.use("/api/oauth",socialAuthRouter)
 //global error handler
 app.use((err:any, req: Request, res:Response, next: NextFunction) => {
     console.error(err);
